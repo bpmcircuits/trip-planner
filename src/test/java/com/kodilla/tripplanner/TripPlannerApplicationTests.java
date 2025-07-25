91c5f4dff03649fe8a50f5b977761bdd5301c1d9
Bartosz Pokrywka <coverbart@gmail.com>
2025-07-25 11:24:41 +0200

    first commit

---
+++ src/test/java/com/kodilla/tripplanner/TripPlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kodilla.tripplanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TripPlannerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
